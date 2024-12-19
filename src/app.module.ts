74719283e9363821b265ffce34796bfaad87cd0e
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2024-12-18 22:16:57 -0600

    Initialize project, install TypeORM for mysql and added ConfigModule to use file environments

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { MesasModule } from './mesas/mesas.module';
+import { MensajesModule } from './mensajes/mensajes.module';
+import { WebsocketModule } from './websocket/websocket.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`, // Carga según el entorno
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'chat_app',
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'development', // Solo sincroniza en desarrollo
+    }),
+    
+    
+    
+    UsuariosModule, MesasModule, MensajesModule, WebsocketModule],
+  controllers: [AppController],
+  providers: [AppService],
+  
+})
+export class AppModule {}
